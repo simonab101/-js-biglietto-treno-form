255d6551c9ab69964ee495faf8526a11fcd01b39
simonab101 <simo.buscicchio@gmail.com>
2023-03-21 15:04:47 +0100

    [add] Ottengo gli elementi html

---
+++ scrypt.js
@@ -0,0 +1,13 @@
+// 1 Ottengo gli elementi html
+const nameField = document.getElementById('name');
+const kmsField = document.getElementById('kms');
+const ageField = document.getElementById('age');
+const generateBtn = document.getElementById('generate-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+const quoteSection = document.getElementById('ticket-quote');
+const namePlaceholder = document.getElementById('passenger-name');
+const offerPlaceholder = document.getElementById('offer');
+const cpPlaceholder = document.getElementById('cp');
+const carriagePlaceholder = document.getElementById('carriage');
+const pricePlaceholder = document.getElementById('price');
